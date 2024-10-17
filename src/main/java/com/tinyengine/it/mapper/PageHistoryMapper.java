0e70a3eec2abe9d0b137dc0046590ee24eacc252
lu17301156525 <lu17301156525@163.com>
2024-10-17 03:15:50 -0700

    Automatically generate code through tables

---
+++ src/main/java/com/tinyengine/it/mapper/PageHistoryMapper.java
@@ -0,0 +1,43 @@
+package com.tinyengine.it.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.tinyengine.it.model.entity.PageHistory;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface PageHistoryMapper extends BaseMapper<PageHistory> {
+
+    /**
+    *  查询表t_page_history所有信息
+    */
+    List<PageHistory> findAllPageHistory();
+
+    /**
+    * 根据主键id查询表t_page_history数据
+    * @param id
+    */
+    PageHistory findPageHistoryById(@Param("id") Integer id);
+    /**
+    *  根据条件查询表t_page_history数据
+    *  @param pageHistory
+    */
+    List<PageHistory> findPageHistoryByCondition(PageHistory pageHistory);
+
+    /**
+    *  根据主键id删除表t_page_history数据
+    *  @param id
+    */
+    Integer deletePageHistoryById(@Param("id") Integer id);
+
+    /**
+    *  根据主键id更新表t_page_history数据
+    *  @param pageHistory
+    */
+    Integer updatePageHistoryById(PageHistory pageHistory);
+
+    /**
+    *  新增表t_page_history数据
+    *  @param pageHistory
+    */
+    Integer createPageHistory(PageHistory pageHistory);
+}
