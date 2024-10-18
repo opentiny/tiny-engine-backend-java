4afeb283bdca4ffff6c76c5c1b226ba4b7d464b5
lu17301156525 <lu17301156525@163.com>
2024-10-18 04:28:23 -0700

    refactor: app extension

--- src/main/java/com/tinyengine/it/config/FilterConfig.java
+++ src/main/java/com/tinyengine/it/config/FilterConfig.java
@@ -3,14 +3,17 @@ package com.tinyengine.it.config;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.tinyengine.it.utils.PermissionInterceptor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
-public class MybatisPlusConfig {
+public class FilterConfig  {
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
@@ -22,6 +25,22 @@ public class MybatisPlusConfig {
     @Bean
     public PermissionInterceptor roleParameterInterceptor() {
         return new PermissionInterceptor();
+    }
+
+
+    @Bean
+    public FilterRegistrationBean<RequestIdFilter> requestIdFilter() {
+        FilterRegistrationBean<RequestIdFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new RequestIdFilter());
+        registrationBean.addUrlPatterns("/*"); // 对所有请求生效
+        return registrationBean;
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        return mapper;
     }
 
 }
