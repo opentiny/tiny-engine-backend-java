eaa2acb723eb2c0bd06f81c8266b657bf6920529
lu17301156525 <lu17301156525@163.com>
2024-11-13 23:43:20 -0800

    fix:format code and fix style issue

--- base/src/main/java/com/tinyengine/it/model/dto/EntriesItem.java
+++ base/src/main/java/com/tinyengine/it/model/dto/EntriesItem.java
@@ -4,9 +4,15 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.Map;
+
+/**
+ * The type EntriesItem.
+ *
+ * @since 2024-10-20
+ */
 @Setter
 @Getter
 public class EntriesItem {
     private Integer lang;
-    private Map<String,Object> entries;
+    private Map<String, Object> entries;
 }
--- base/src/main/java/com/tinyengine/it/model/entity/MaterialComponent.java
+++ base/src/main/java/com/tinyengine/it/model/entity/MaterialComponent.java
@@ -3,6 +3,7 @@ package com.tinyengine.it.model.entity;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
