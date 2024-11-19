65b5fe4bbda8a0f87f6d94d5ae7be01f9a380e55
zjc <1569294420@qq.com>
2024-11-19 00:01:56 -0800

    fix： fix the functionality of the group query interface

--- base/src/main/java/com/tinyengine/it/mapper/BlockGroupMapper.java
+++ base/src/main/java/com/tinyengine/it/mapper/BlockGroupMapper.java
@@ -20,10 +20,18 @@ import java.util.List;
  */
 public interface BlockGroupMapper extends BaseMapper<BlockGroup> {
     /**
+     * 查询表t_block_group所有信息及关联的区块信息
+     *
+     * @return the list
+     */
+    List<BlockGroup> queryAllBlockGroupAndBlock(String createdBy);
+
+    /**
      * 查询表t_block_group所有信息
      *
      * @return the list
      */
+    @Select("select * from t_block_group ")
     List<BlockGroup> queryAllBlockGroup();
 
     /**
@@ -32,6 +40,15 @@ public interface BlockGroupMapper extends BaseMapper<BlockGroup> {
      * @param id the id
      * @return the block group
      */
+    BlockGroup queryBlockGroupAndBlockById(@Param("id") Integer id, String createdBy);
+
+    /**
+     * 通过ID查询分组信息
+     *
+     * @param id id
+     * @return the list
+     */
+    @Select("select * from t_block_group bg where bg.id = #{id}")
     BlockGroup queryBlockGroupById(@Param("id") Integer id);
 
     /**
@@ -67,11 +84,20 @@ public interface BlockGroupMapper extends BaseMapper<BlockGroup> {
     Integer createBlockGroup(BlockGroup blockGroup);
 
     /**
-     * 通过appId查区块分组
+     * 通过appId和createdBy查区块分组关联的区块信息
      *
      * @param appId the app id
      * @return the list
      */
+    List<BlockGroup> queryBlockGroupByAppId(Integer appId, String createdBy);
+
+    /**
+     * 通过appId获取分组信息
+     *
+     * @param appId appId
+     * @return the list
+     */
+    @Select("select * from t_block_group bg where bg.app_id = #{appId}")
     List<BlockGroup> queryBlockGroupByApp(Integer appId);
 
     /**
@@ -94,75 +120,6 @@ public interface BlockGroupMapper extends BaseMapper<BlockGroup> {
             + "WHERE bg.id = #{blockGroupId} "
             + "GROUP BY bg.id")
     List<BlockGroupDto> getBlockGroupsById(int blockGroupId);
-
-    /**
-     * 查询区块分组信息以及关联的app和block信息
-     *
-     * @return the list
-     */
-    @Results({
-            @Result(column = "app", property = "appId"),
-            @Result(column = "block_group_id", javaType = List.class, property = "blocks",
-                    many = @Many(select = "com.tinyengine.it.mapper.BlockMapper.findBlocksByBlockGroupId")),
-            @Result(column = "app", property = "app",
-                    one = @One(select = "com.tinyengine.it.mapper.AppMapper.queryAppById"))
-    })
-    @Select("SELECT bg.*, bs.block_group_id as block_group_id, a.id as app  "
-            + "FROM t_block_group bg "
-            + "left join t_app a on bg.app_id = a.id "
-            + "left join t_block bs on bg.id = bs.block_group_id "
-            + "GROUP BY bg.id")
-    List<BlockGroupDto> getBlockGroups();
-
-
-    /**
-     * 根据blockGroupIds获取区块分组信息以及关联的app和block信息
-     *
-     * @param ids the ids
-     * @return the list
-     */
-    @Results({
-            @Result(column = "app", property = "appId"),
-            @Result(column = "app", property = "app",
-                    one = @One(select = "com.tinyengine.it.mapper.AppMapper.queryAppById")),
-            @Result(column = "block_group_id", javaType = List.class, property = "blocks",
-                    many = @Many(select = "com.tinyengine.it.mapper.BlockMapper.findBlocksByBlockGroupId"))
-    })
-    @Select("<script>"
-            + "SELECT bg.*, bs.block_group_id as block_group_id, a.id as app  "
-            + "FROM t_block_group bg "
-            + "left join t_app a on bg.app_id = a.id "
-            + "left join t_block bs on bg.id = bs.block_group_id "
-            + "WHERE bg.id in "
-            + "<foreach item='id' collection='ids' open='(' separator=',' close=')'>"
-            + "#{id}"
-            + "</foreach>"
-            + "GROUP BY bg.id"
-            + "</script>"
-    )
-    List<BlockGroupDto> getBlockGroupsByIds(List<Integer> ids);
-
-
-    /**
-     * 根据appId获取区块分组信息以及关联的app和block信息
-     *
-     * @param appId the app id
-     * @return the list
-     */
-    @Results({
-            @Result(column = "app", property = "appId"),
-            @Result(column = "app", property = "app",
-                    one = @One(select = "com.tinyengine.it.mapper.AppMapper.queryAppById")),
-            @Result(column = "block_group_id", javaType = List.class, property = "blocks",
-                    many = @Many(select = "com.tinyengine.it.mapper.BlockMapper.findBlocksByBlockGroupId"))
-    })
-    @Select("select bg.*, bs.block_group_id as block_group_id, a.id as app "
-            + "from t_block_group bg "
-            + "left join t_app a on bg.app_id = a.id "
-            + "left join t_block bs on bg.id = bs.block_group_id "
-            + "where bg.app_id = #{appId} "
-            + "GROUP BY bg.id")
-    List<BlockGroupDto> findGroupByAppId(int appId);
 
 
     /**
