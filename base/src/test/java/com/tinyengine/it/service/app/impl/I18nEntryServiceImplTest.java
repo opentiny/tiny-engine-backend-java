0e1438967087f21578c76f4e4983981ef38f89f6
lu-yg <128358973+lu-yg@users.noreply.github.com>
2024-11-21 15:01:31 +0800

    fix: format code and fix style issue (#107)
    * fix: format code and fix style issue
    
    * fix: format code and fix style issue

--- base/src/test/java/com/tinyengine/it/service/app/impl/I18nEntryServiceImplTest.java
+++ base/src/test/java/com/tinyengine/it/service/app/impl/I18nEntryServiceImplTest.java
@@ -15,7 +15,16 @@ import com.tinyengine.it.common.base.Result;
 import com.tinyengine.it.common.utils.Utils;
 import com.tinyengine.it.mapper.I18nEntryMapper;
 import com.tinyengine.it.mapper.I18nLangMapper;
-import com.tinyengine.it.model.dto.*;
+import com.tinyengine.it.model.dto.DeleteI18nEntry;
+import com.tinyengine.it.model.dto.EntriesItem;
+import com.tinyengine.it.model.dto.Entry;
+import com.tinyengine.it.model.dto.FileResult;
+import com.tinyengine.it.model.dto.I18nEntryDto;
+import com.tinyengine.it.model.dto.I18nEntryListResult;
+import com.tinyengine.it.model.dto.JsonFile;
+import com.tinyengine.it.model.dto.OperateI18nBatchEntries;
+import com.tinyengine.it.model.dto.OperateI18nEntries;
+import com.tinyengine.it.model.dto.SchemaI18n;
 import com.tinyengine.it.model.entity.I18nEntry;
 import com.tinyengine.it.model.entity.I18nLang;
 
