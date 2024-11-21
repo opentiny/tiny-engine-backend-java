5e16fe060b91cefe0869075a510985c8772eb32c
lu17301156525 <lu17301156525@163.com>
2024-11-20 22:12:48 -0800

    fix: format code and fix style issue

--- base/src/main/java/com/tinyengine/it/model/entity/Tenant.java
+++ base/src/main/java/com/tinyengine/it/model/entity/Tenant.java
@@ -1,8 +1,11 @@
 package com.tinyengine.it.model.entity;
 
-import com.baomidou.mybatisplus.annotation.*;
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.tinyengine.it.common.base.BaseEntity;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
