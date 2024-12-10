3a35d9c7adc92d20b1c71bbb7e365ffa8103ba5c
lu17301156525 <lu17301156525@163.com>
2024-12-09 19:49:51 -0800

    fix: data source create bug

--- base/src/main/java/com/tinyengine/it/service/app/impl/AppServiceImpl.java
+++ base/src/main/java/com/tinyengine/it/service/app/impl/AppServiceImpl.java
@@ -1,13 +1,12 @@
 /**
  * Copyright (c) 2023 - present TinyEngine Authors.
  * Copyright (c) 2023 - present Huawei Cloud Computing Technologies Co., Ltd.
- *
+ * <p>
  * Use of this source code is governed by an MIT-style license.
- *
+ * <p>
  * THE OPEN SOURCE SOFTWARE IN THIS PRODUCT IS DISTRIBUTED IN THE HOPE THAT IT WILL BE USEFUL,
  * BUT WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF MERCHANTABILITY OR FITNESS FOR
  * A PARTICULAR PURPOSE. SEE THE APPLICABLE LICENSES FOR MORE DETAILS.
- *
  */
 
 package com.tinyengine.it.service.app.impl;
@@ -157,7 +156,7 @@ public class AppServiceImpl implements AppService {
     @SystemServiceLog(description = "应用修改实现方法")
     public Result<App> updateAppById(App app) {
         // 如果更新extend_config字段，从platform获取数据，继承非route部分
-        if (!app.getExtendConfig().isEmpty()) {
+        if (app.getExtendConfig() != null && !app.getExtendConfig().isEmpty()) {
             App appResult = appMapper.queryAppById(app.getId());
             Platform platform = platformService.queryPlatformById(appResult.getPlatformId());
             Map<String, Object> appExtendConfig = platform.getAppExtendConfig();
