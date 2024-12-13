418a303ba18e6ece68ad43a53838cea10fbd52ba
coderliux <ecliujie@gmail.com>
2024-12-13 00:42:08 -0800

    add class tag

--- base/src/main/java/com/tinyengine/it/controller/BlockGroupController.java
+++ base/src/main/java/com/tinyengine/it/controller/BlockGroupController.java
@@ -26,6 +26,7 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
@@ -52,6 +53,7 @@ import javax.validation.Valid;
 @Validated
 @RestController
 @RequestMapping("/material-center/api")
+@Tag(name= "区块")
 public class BlockGroupController {
     @Autowired
     private BlockGroupService blockGroupService;
--- base/src/main/java/com/tinyengine/it/controller/v1/AppV1Controller.java
+++ base/src/main/java/com/tinyengine/it/controller/v1/AppV1Controller.java
@@ -1,13 +1,12 @@
 /**
  * Copyright (c) 2023 - present TinyEngine Authors.
  * Copyright (c) 2023 - present Huawei Cloud Computing Technologies Co., Ltd.
- *
+ * <p>
  * Use of this source code is governed by an MIT-style license.
- *
+ * <p>
  * THE OPEN SOURCE SOFTWARE IN THIS PRODUCT IS DISTRIBUTED IN THE HOPE THAT IT WILL BE USEFUL,
  * BUT WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF MERCHANTABILITY OR FITNESS FOR
  * A PARTICULAR PURPOSE. SEE THE APPLICABLE LICENSES FOR MORE DETAILS.
- *
  */
 
 package com.tinyengine.it.controller.v1;
@@ -23,6 +22,7 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
@@ -39,6 +39,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Validated
 @RestController
 @RequestMapping("/app-center/v1/api")
+@Tag(name= "应用")
 public class AppV1Controller {
     @Autowired
     private AppV1Service appV1Service;
