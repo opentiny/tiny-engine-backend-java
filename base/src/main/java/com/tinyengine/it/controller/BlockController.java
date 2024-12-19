ee090279f3c2603570b2e75fd5fbf27d66602c59
lu17301156525 <lu17301156525@163.com>
2024-12-18 22:54:44 -0800

    feat: block current history

--- base/src/main/java/com/tinyengine/it/controller/BlockController.java
+++ base/src/main/java/com/tinyengine/it/controller/BlockController.java
@@ -1,12 +1,13 @@
 /**
  * Copyright (c) 2023 - present TinyEngine Authors.
  * Copyright (c) 2023 - present Huawei Cloud Computing Technologies Co., Ltd.
- * <p>
+ *
  * Use of this source code is governed by an MIT-style license.
- * <p>
+ *
  * THE OPEN SOURCE SOFTWARE IN THIS PRODUCT IS DISTRIBUTED IN THE HOPE THAT IT WILL BE USEFUL,
  * BUT WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF MERCHANTABILITY OR FITNESS FOR
  * A PARTICULAR PURPOSE. SEE THE APPLICABLE LICENSES FOR MORE DETAILS.
+ *
  */
 
 package com.tinyengine.it.controller;
@@ -49,9 +50,8 @@ import java.util.Map;
 import javax.validation.Valid;
 
 /**
- * <p>
+ *
  * 区块
- * </p>
  *
  * @author zhangjuncao
  * @since 2024-10-30
