1118473ca1b5810b0954031be69c5c3f7cfcbda6
lu-yg <128358973+lu-yg@users.noreply.github.com>
2024-12-20 00:55:56 -0800

    feat: block deploy (#139)
    * fix: format code and fix style issue
    
    * fix: format code and fix style issue
    
    * fix: format code and fix style issue
    
    * feat: page deploy
    
    * feat: page history find
    
    * fix: component TinyInput sql
    
    * fix: data source create bug
    
    * fix: data source create bug
    
    * fix: component insert sql
    
    * fix: data source create bug
    
    * fix: data source create bug
    
    * feat: block current history
    
    * feat: block current history
    
    * feat: block current history
    
    * fix: code style format
    
    * fix: code style format
    
    * feat: block deploy

---
+++ base/src/main/java/com/tinyengine/it/model/dto/BlockBuildDto.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2023 - present TinyEngine Authors.
+ * Copyright (c) 2023 - present Huawei Cloud Computing Technologies Co., Ltd.
+ *
+ * Use of this source code is governed by an MIT-style license.
+ *
+ * THE OPEN SOURCE SOFTWARE IN THIS PRODUCT IS DISTRIBUTED IN THE HOPE THAT IT WILL BE USEFUL,
+ * BUT WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF MERCHANTABILITY OR FITNESS FOR
+ * A PARTICULAR PURPOSE. SEE THE APPLICABLE LICENSES FOR MORE DETAILS.
+ *
+ */
+
+package com.tinyengine.it.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+
+/**
+ * The type BlockBuildDto.
+ *
+ * @since 2024-12-19
+ */
+@Data
+public class BlockBuildDto {
+    private BlockDto block;
+    @JsonProperty("deploy_info")
+    private String deployInfo;
+    @JsonProperty("is_compile")
+    private Boolean isCompile;
+    private Boolean needToSave;
+    private String version;
+}
