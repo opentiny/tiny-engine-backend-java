7f875cee03124475d63c50f33ffec9bea039a4ef
lu17301156525 <lu17301156525@163.com>
2025-05-12 19:27:37 -0700

    feat: platform API

--- app/src/main/resources/sql/mysql/update_all_tables_ddl.sql
+++ app/src/main/resources/sql/mysql/update_all_tables_ddl.sql
@@ -5,3 +5,4 @@ ALTER TABLE t_datasource DROP INDEX u_idx_datasource;
 ALTER TABLE t_datasource ADD INDEX u_idx_datasource (`tenant_id`, `platform_id`, `name`, `app_id`);
 
 ALTER TABLE t_platform_history MODIFY sub_count int NULL;
+ALTER TABLE t_platform_history MODIFY publish_url varchar(255) NULL;
