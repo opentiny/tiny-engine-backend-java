68b329a1a672fb34fe4f4f017bbcc8f6e1ce8bed
lu17301156525 <lu17301156525@163.com>
2025-05-13 02:31:37 -0700

    feat: Add inspection file

--- base/src/main/java/com/tinyengine/it/controller/PlatformHistoryController.java
+++ base/src/main/java/com/tinyengine/it/controller/PlatformHistoryController.java
@@ -28,6 +28,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -132,7 +133,7 @@ public class PlatformHistoryController {
                             schema = @Schema(implementation = PlatformHistory.class))),
             @ApiResponse(responseCode = "400", description = "请求失败")})
     @SystemControllerLog(description = "修改单个PlatformHistory信息")
-    @PostMapping("/platform-history/update/{id}")
+    @PutMapping("/platform-history/update/{id}")
     public Result<PlatformHistory> updatePlatformHistory(@PathVariable Integer id, @RequestBody PlatformHistory platformHistory) {
         platformHistory.setId(id);
         return platformHistoryService.updatePlatformHistoryById(platformHistory);
