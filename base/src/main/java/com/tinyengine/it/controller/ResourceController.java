0b8105c87d6ab7f9803c89d236cd85697a23e372
lu17301156525 <lu17301156525@163.com>
2025-09-08 19:06:23 -0700

    fix: modify resource download

--- base/src/main/java/com/tinyengine/it/controller/ResourceController.java
+++ base/src/main/java/com/tinyengine/it/controller/ResourceController.java
@@ -273,7 +273,7 @@ public class ResourceController {
         String detectedType = ImageThumbnailGenerator.detectFormatFromBase64(base64Data);
         String fileExtension = detectedType.equals("jpeg") ? "jpg" : detectedType;
 
-        String fileName = useOriginal ? resource.getName() : resource.getName() + "_thumbnail.";
+        String fileName = useOriginal ? resource.getName() : "thumbnail_" + resource.getName();
         // URL编码文件名
         String encodedFileName = URLEncoder.encode(fileName, StandardCharsets.UTF_8)
                 .replace("+", "%20");
