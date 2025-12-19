eae40931af0863a18c373b92081cdac19a839b3a
lu17301156525 <lu17301156525@163.com>
2025-12-18 22:08:50 -0800

    fix: modify AI API

--- base/src/main/java/com/tinyengine/it/controller/AiChatController.java
+++ base/src/main/java/com/tinyengine/it/controller/AiChatController.java
@@ -27,7 +27,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
